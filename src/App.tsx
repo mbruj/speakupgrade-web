3891ecc7b01fae08214ad35b46a7e6afee0ee414
howtosavemytime-sys <howtosavemytime@gmail.com>
2026-05-26 15:27:33 +0200

    Add Mira to setup page, compact daily challenge, MiraEditPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store'
-
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import SetupPage from './pages/SetupPage'
@@ -13,6 +12,7 @@ import ResultsPage from './pages/ResultsPage'
 import UpgradePage from './pages/UpgradePage'
 import HistoryPage from './pages/HistoryPage'
 import { AffiliatePage, FeedbackPage } from './pages/AffiliateFeedbackPages'
+import MiraEditPage from './pages/MiraEditPage'
 
 function RequireAuth({ children }: { children: React.ReactNode }) {
   const email = useAuthStore((s) => s.email)
@@ -25,7 +25,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-
         <Route path="/setup" element={<RequireAuth><SetupPage /></RequireAuth>} />
         <Route path="/instructions" element={<RequireAuth><InstructionsPage /></RequireAuth>} />
         <Route path="/permissions" element={<RequireAuth><PermissionsPage /></RequireAuth>} />
@@ -37,7 +36,7 @@ export default function App() {
         <Route path="/history" element={<RequireAuth><HistoryPage /></RequireAuth>} />
         <Route path="/affiliate" element={<RequireAuth><AffiliatePage /></RequireAuth>} />
         <Route path="/feedback" element={<RequireAuth><FeedbackPage /></RequireAuth>} />
-
+        <Route path="/mira-edit" element={<RequireAuth><MiraEditPage /></RequireAuth>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
