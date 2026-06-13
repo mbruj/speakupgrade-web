1c8394de62ac38764ccc83e376946a4abcefe892
howtosavemytime-sys <howtosavemytime@gmail.com>
2026-06-13 17:09:45 +0200

    Cap Mira message at 300 chars, keep box at 200px

--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -136,7 +136,7 @@ export default function SetupPage() {
     })
       .then(r => r.json())
       .then(data => {
-        if (data.message) setMiraMessage(data.message.length > 380 ? data.message.slice(0, 377) + '...' : data.message)
+        if (data.message) setMiraMessage(data.message.length > 300 ? data.message.slice(0, 297) + '...' : data.message)
         setMiraOnboarded(data.is_onboarded || false)
       })
       .catch(console.error)
